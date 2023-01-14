d33e337c46353ad4bac22893d0e51ad93ebd6a6e
Rakhim9830 <106856795+Rakhim9830@users.noreply.github.com>
2023-01-14 17:37:18 +0600

    Initial commit

---
+++ app/src/main/java/com/rahim/lesson1/ui/Task/Task.kt
@@ -0,0 +1,41 @@
+package com.rahim.lesson1.ui.Task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+import com.rahim.lesson1.R
+import com.rahim.lesson1.databinding.FragmentTaskBinding
+import com.rahim.lesson1.ui.TaskData
+
+
+class Task : Fragment() {
+
+private lateinit var binding: FragmentTaskBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+     binding = FragmentTaskBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSave.setOnClickListener{
+            setFragmentResult(RESULT_TASK, bundleOf("bundleKey" to TaskData(binding.edTitle.text.toString(), binding.edDesc.text.toString())))
+            findNavController().navigateUp()
+        }
+
+    }
+    companion object{
+        const val RESULT_TASK = "result"
+    }
+}
---
+++ app/src/main/java/com/rahim/lesson1/ui/TaskData.kt
@@ -0,0 +1,9 @@
+package com.rahim.lesson1.ui
+
+import java.io.Serializable
+
+data  class TaskData (
+  var title:String? = null,
+     val desc:String? = null
+
+) : Serializable
